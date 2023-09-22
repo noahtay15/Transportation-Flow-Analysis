fada5a859ba595af7d1cc48170adb65218758756
Noah Taylor <93206722+noahtay15@users.noreply.github.com>
2023-09-22 19:16:51 +0000

    Test

--- Transportation-Flow-Analysis/script.py
+++ Transportation-Flow-Analysis/script.py
@@ -7,6 +7,6 @@ map = fol.Map(location=(40.387778, 96.410278),
         control_scale=True, 
         zoom_control=False, 
         )
-downloadsDir = os.path.expanduser("~" + os.sep + "Downloads")
-html = os.path.join(downloadsDir, "map.html")
-map.save(html)
+#downloadsDir = os.path.expanduser("~" + os.sep + "Downloads")
+#html = os.path.join(downloadsDir, "map.html")
+map.save("map.html")
