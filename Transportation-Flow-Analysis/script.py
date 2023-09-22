ebdd7dcad85651556bc0a1e79eb4b1c312e8ff2b
Noah Taylor <93206722+noahtay15@users.noreply.github.com>
2023-09-22 20:18:58 +0000

    Working on Chloropleth

--- Transportation-Flow-Analysis/script.py
+++ Transportation-Flow-Analysis/script.py
@@ -1,12 +1,25 @@
 import pandas as pd
 import folium as fol
 import os
+import requests
+import json
 
 
-map = fol.Map(location=(40.387778, 96.410278),
+map = fol.Map(location=(40, -100),
         control_scale=True, 
         zoom_control=False, 
-        )
+        zoom_start=6)
+
+with open('us-counties.json', 'r') as countiesJson:
+    USCounties = json.load(countiesJson)
+
+fol.Choropleth(
+    geo_data=USCounties, 
+    fill_opacity=0.3, 
+    line_weight=2
+).add_to(map)
+
+
 #downloadsDir = os.path.expanduser("~" + os.sep + "Downloads")
 #html = os.path.join(downloadsDir, "map.html")
 map.save("map.html")
