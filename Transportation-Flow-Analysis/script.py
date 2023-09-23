694a9fd6e92b670e19c9c71880f6ab780d4b10dc
Noah Taylor <93206722+noahtay15@users.noreply.github.com>
2023-09-23 20:32:34 +0000

    Put the county population data in the chloropleth

--- Transportation-Flow-Analysis/script.py
+++ Transportation-Flow-Analysis/script.py
@@ -1,6 +1,12 @@
 import json
 import pandas as pd
 import folium as fol
+import openpyxl
+
+
+populationDataframe = pd.read_excel('Somewhat Fixed 2.xlsx', sheet_name="CO-EST2022-POP", skiprows=2, usecols="C:F", nrows=3100)
+populationDataframe.set_index('id', inplace=True)
+print(populationDataframe)
 
 
 m = fol.Map(location=(40, -100),
@@ -11,18 +17,23 @@ m = fol.Map(location=(40, -100),
 with open('us-counties.json', 'r') as countiesJson:
     USCounties = json.load(countiesJson)
 
-countyPopChloro = fol.Choropleth(
-    geo_data=USCounties, 
-    fill_opacity=0.3, 
-    line_weight=2, 
-    data=None, #change later
-    columns=['id', 'name'],
-    key_on='feature.id',
-    fill_color='YlGnBu', 
-    name='Population by County'
-).add_to(m)
 
-""" Add later
+#County Population
+for year in ['2020 Population', '2021 Population', '2022 Population']:
+    fol.Choropleth(
+        geo_data=USCounties, 
+        fill_opacity=1, 
+        line_weight=2, 
+        data=populationDataframe,
+        columns=[populationDataframe.index, year],
+        threshold_scale=[0, 50000, 100000, 150000, 200000, 10000000],
+        key_on='feature.id',
+        fill_color='YlGnBu', 
+        name=f'{year} by County'
+    ).add_to(m)
+
+
+"""
 stateLoadsChloro = fol.Choropleth(
     
 ).add_to(m)
