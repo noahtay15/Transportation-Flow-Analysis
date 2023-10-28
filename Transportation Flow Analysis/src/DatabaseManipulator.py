f9345d04654f0843d50dc0ffcbd29fcfbf756fcc
Noah Taylor <93206722+noahtay15@users.noreply.github.com>
2023-10-28 16:33:52 +0000

    Documentaion to mapmaker, add methods to dbman

--- Transportation Flow Analysis/src/DatabaseManipulator.py
+++ Transportation Flow Analysis/src/DatabaseManipulator.py
@@ -1,9 +1,64 @@
 """
-TODO: 
-    - Everything
+TODO:
 """
-
+import sqlite3 as sql
 
 class DatabaseManipulator():
-    def __init__(self):
+    def __init__(self, path):
         
+        """
+        Initialize a DatabaseManipulator object and connect to a SQLite database.
+
+        Args:
+            path (str): The path to the SQLite database file to connect to.
+        """
+        
+        self.con = sql.connect(path)
+        self.cur = self.con.cursor()
+        
+    def execute_command(self, command):
+        
+        """
+        Execute an SQL command and commit the changes to the database.
+
+        Args:
+            command (str): The SQL command to be executed.
+        """
+        
+        self.cur.execute(command)
+        self.con.commit()
+    
+    def fetch_data(self, command):
+        
+        """
+        Execute an SQL query and fetch the resulting data.
+
+        Args:
+            command (str): The SQL query to be executed.
+
+        Returns:
+            list of tuples: A list containing the retrieved data as tuples.
+        """
+        
+        res = self.cur.execute(command)
+        return res.fetchall()
+    
+    def fill_table(self, command, data):
+        
+        """
+        Execute an SQL command to insert multiple rows of data into a table.
+
+        Args:
+            command (str): The SQL command to insert data into the table.
+            data (list of tuples): Data to be inserted into the table.
+        """
+        
+        self.cur.executemany(command, data)
+        self.con.commit()
+        
+    def close_con(self):
+        
+        """
+        Closes the database connection
+        """
+        self.con.close()
--- Transportation Flow Analysis/src/MapMaker.py
+++ Transportation Flow Analysis/src/MapMaker.py
@@ -1,13 +1,31 @@
 """
 TODO:
-    - Everything
 """
 
 import folium as fol
-import DatabaseManipulator as dbMan
 
 class MapMaker():
     def __init__(self):
+        
+        """
+        Initialize a MapMaker object with default map settings.
+
+        This constructor sets up a Folium map with default parameters, including a specified
+        location, zoom level, map boundaries, and tileset. It provides a starting point for
+        further customization and adding layers to the map.
+
+        Attributes:
+            map (folium.Map): The Folium map object.
+
+        Default Parameters:
+            - Location: Latitude 40, Longitude -100
+            - Map Controls: Scale control enabled, zoom control disabled
+            - Zoom Level: 5
+            - Minimum Zoom Level: 5
+            - Maximum Bounds: Set to a region within longitude -130 to -60 and latitude 20 to 52.
+            - Tiles: "Cartodb Positron" tileset
+        """
+        
         self.map = fol.Map(location=(40, -100),
                             control_scale=True, 
                             zoom_control=False, 
@@ -20,7 +38,9 @@ class MapMaker():
                             max_lat=52,
                             tiles="Cartodb Positron")
 
-    """
+    def add_layer(self, dataFrame, geodata, dataColumn, fillColor, name, show, nameColumn):
+        
+        """
         Add a choropleth layer, with tooltip, to the map with specified data and
         style and adds it to the map object
 
@@ -36,7 +56,7 @@ class MapMaker():
         Returns:
             None
         """
-    def add_layer(self, dataFrame, geodata, dataColumn, fillColor, name, show, nameColumn):
+        
         layer = fol.Choropleth(
             geo_data=geodata, 
             fill_opacity=1, 
@@ -56,6 +76,20 @@ class MapMaker():
         layer.add_to(self.map)
 
     def add_tooltip(self, layer, dataFrame, dataColumn, nameColumn):
+        
+        """
+        A helper method for adding tooltips to a choropleth layer for displaying additional information.
+
+        Args:
+            layer (folium.Choropleth): The choropleth layer to which tooltips will be added.
+            dataFrame (pandas.DataFrame): The data source for tooltip information.
+            dataColumn (str): The column in the dataFrame used for data display in the tooltip.
+            nameColumn (str): The column in the dataFrame used for feature names in the tooltip.
+
+        Returns:
+            folium.GeoJsonTooltip: The tooltip configuration added to the choropleth layer.
+        """
+        
         for s in layer.geojson.data['features']:
             feature_id = int(s['id'])  # Convert the GeoJSON id to an integer
             s['properties'][dataColumn] = int(dataFrame.loc[feature_id, dataColumn])
@@ -64,8 +98,17 @@ class MapMaker():
         return fol.GeoJsonTooltip([nameColumn, dataColumn])
 
     def get_map(self):
+        
+        """
+        returns self.map
+        """
+        
         return self.map       
-        
-    
-        
-        
+
+    def add_layer_control(self):
+        """
+        Adds layer controls to the map. This should be done after
+        all the layers have been added to the map, or the layers
+        may not show up correctly on the toggle tool.
+        """
+        fol.LayerControl.add_to(self.map)
